4d7359563a784d0530aac3928c84d2c866f18b55
Mangood <manxiao@xdf.cn>
2022-11-18 12:31:27 +0800

    setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 this_directory = os_path.abspath(os_path.dirname(__file__))
 URL = 'https://github.com/MangoodLuck/django-rbac-plus.git'
 NAME = 'django-rbac-plus'
-VERSION = '1.0'
+VERSION = '1.0.0'
 DESCRIPTION = 'rbac permission plus'
 if os.path.exists('README.md'):
     with open('README.md', encoding='utf-8') as f:
